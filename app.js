0b98c7d038c89aeb7e43a149fee6abfd96127e98
Zair <rzpeguero@truora.com>
2026-04-20 16:07:54 -0400

    fix: puerto dinámico para render

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.get("/", (req, res) => {
 module.exports = app;
 
 if (require.main === module) {
-  app.listen(3000, () => {
-    console.log("Servidor corriendo en puerto 3000");
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
   });
 }
